f034307dc8ce4e9e584f69d665d6c909f8d1877a
Jakob <jakob_gauffin@hotmail.com>
2020-09-13 23:07:08 +0200

    Add target modal

--- src/components/Vimeo/Vimeo.jsx
+++ src/components/Vimeo/Vimeo.jsx
@@ -15,10 +15,19 @@ const [play, setPlay] = useState(false);
     />
         </div>
       </div>
+      <div className="Vimeo__ButtonWrapper">
+        <a href="#modal" className="Vimeo__Button Vimeo__Button--Modal">
+          Modal
+        </a>
+        <a className="Vimeo__Button Vimeo__Button--Play" onClick={() => setPlay(!play)}>
+          {play ? "Pause" : "Play"}
+        </a>
+      </div>
 
-      <button className="Vimeo__Button" onClick={() => setPlay(!play)}>
-        {play ? "Pause" : "Play"}
-      </button>
+      <div id="modal" className="Vimeo__Modal">
+        <a href="#" className="Vimeo__Close">&times;</a>
+      </div>
+      <a href="#" className="Vimeo__Backdrop"></a>
 {/* <iframe src="https://player.vimeo.com/video/202660709?portrait=0" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
 <p><a href="https://vimeo.com/202660709">Paradise Waterfall</a> from <a href="https://vimeo.com/kreolyab">Christian CUVELIER</a> on <a href="https://vimeo.com">Vimeo</a>.</p> */}
       
